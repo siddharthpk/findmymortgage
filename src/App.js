8696465c116b888b22f00741399a08dd888f8f55
Siddharth Pathak <siddharthpathak@uvic.ca>
2023-06-18 23:13:23 -0700

    slider, tenure, result, sub components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import Navbar from "./components/Navbar";
+import Result from "./components/Result";
+import SliderSelect from "./components/SliderSelect";
+import TenureSelect from "./components/TenureSelect";
 
 function App() {
   return <div className="App">
-    <Navbar /></div>;
+    <Navbar />
+    <Container maxWidth="xl" sx={{marginTop:4}}>
+    <Grid container spacing={5} alignItems="center">
+      <Grid item xs={12} md={6}>
+        <SliderSelect />
+        <TenureSelect />
+      </Grid>
+      <Grid item xs={12} md={6}>
+        <Result/>
+      </Grid>
+    </Grid>
+  </Container> 
+  </div>;
 }
 
 export default App;
